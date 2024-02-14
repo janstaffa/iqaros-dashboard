82e78d854141f7e0b00de83be7ec852fb165f57d
janstaffa <jstaffa@janstaffa.cz>
2024-02-14 11:49:38 +0100

    Minor production changes + reverse proxy for server requests + 404 page

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,7 +1,6 @@
-const SERVER_HOST =
-  process.env.NODE_ENV === 'production' ? 'localhost' : 'localhost';
+const SERVER_HOST = window.location.host; // process.env.NODE_ENV === 'production' ? 'SERVER_HOST' : 'localhost';
 
-const SERVER_PORT = process.env.NODE_ENV === 'production' ? 8080 : 4000;
+const SERVER_PORT = process.env.NODE_ENV === 'production' ? 80 : 4000;
 
 export const API_BASE_PATH = `http://${SERVER_HOST}:${SERVER_PORT}/api`;
 export const APP_API_BASE_PATH = `http://${SERVER_HOST}:${SERVER_PORT}/app`;
--- web/src/pages/NotFound.tsx
+++ web/src/pages/NotFound.tsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+
 export interface NotFoundProps {}
 
 const NotFound: React.FC<NotFoundProps> = () => {
-  return <div className="w-full text-center">404</div>;
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <div className="mb-5">
+        <h1 className="w-full text-center text-9xl text-accent-normal">404</h1>
+        <p className="text-2xl">Stránka nebyla nalezena</p>
+      </div>
+      <Link
+        to="/"
+        className="bg-accent-normal text-white rounded-full px-4 py-2 border-none hover:bg-accent-light"
+      >
+        Zpět
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
