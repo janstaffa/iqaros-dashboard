83313919d8deaee5b897081645c8cb02cbdcfaf5
Jan Štaffa <58565385+janstaffa@users.noreply.github.com>
2024-03-04 17:39:55 +0100

    Update config.ts

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,6 +1,6 @@
 const SERVER_HOST = window.location.host; // process.env.NODE_ENV === 'production' ? 'SERVER_HOST' : 'localhost';
 
-const SERVER_PORT = process.env.NODE_ENV === 'production' ? 80 : 4000;
+const SERVER_PORT = process.env.NODE_ENV === 'production' ? 80 : 4000; // 8080
 
 export const API_BASE_PATH = `http://${SERVER_HOST}:${SERVER_PORT}/api`;
 export const APP_API_BASE_PATH = `http://${SERVER_HOST}:${SERVER_PORT}/app`;
